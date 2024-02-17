54466e57cf4e9535a5ec2d8b9b5dd4650d5d3c80
Danila <danilavasilev281220@gmail.com>
2024-02-17 16:39:16 +0300

    sfdse

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Profile from "./pages/profile";
 import './styles/App.css'
+
 function App() {
   return (
     <div className="App">
--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from "react-router-dom"
 const Profile = () => {
+    const[id, setID] = React.useState('')
+    const[name, setName] = React.useState('')
+    const[classschool, setClassschool] = React.useState('')
+    const[letter, setLetter] = React.useState('')
+    const[phone, setPhone] = React.useState('')
+    const[user_type, setUser_type] = React.useState('')
+    const[birthday, setBirthday] = React.useState('')
+    useEffect(() => {
+        fetch('http://localhost:3001/user')
+        .then(response => response.json())
+        .then(response => {
+            setBirthday(response.birthday)
+            setID(response.id)
+            setName(response.name)
+            setClassschool(response.class)
+            setLetter(response.letter)
+            setPhone(response.phone)
+            setUser_type(response.user_type)  
+           
+            })
+        }, [])
     return (
         <div className='html'>
             <div>
                 <strong  className = 'title'>Профиль</strong>
                 <div className = 'text'>
-                    <div>Id:</div>
-                    <div>Имя:</div>
-                    <div>Класс:</div>
-                    <div>Буква:</div>
-                    <div>День рождения:</div>
-                    <div>Телефон:</div>
-                    <div>Тип пользователя:</div>
+                    
+                    <div>Id: {id}</div>
+                    <div>Имя: {name}</div>
+                    <div>Класс: {classschool}</div>
+                    <div>Буква: {letter}</div>
+                    <div>День рождения: {birthday}</div>
+                    <div>Телефон: {phone}</div>
+                    <div>Тип пользователя: {user_type}</div>
+                    <div>День рождения: {birthday}</div>
                     </div>
                     <div>
                          <h3>
